34347ece56a1737e5c9db962e56ca4c50a2d2e2b
Geert Schippers <g.j.f.m.@gmail.com>
2018-03-14 16:20:54 +0100

    styling in een apart .css bestand

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <html>
+    <head>
+        <link rel="stylesheet" href="style.css">
+    </head>
     <body>
        <?php
             echo "<table>";
                 for($y=0;$y<11;$y++){ 
-                    echo "<tr border='2px' bordercolor='yellow'>";
+                    echo "<tr>";
                             for($x=0;$x<10;$x++){
-                               
-                                    echo "<td bgcolor=#2B83A1 bordercolor='yellow' border='2px' style='height: 50px; width: 50px; text-align:center;'>tex</td>";
+                                    echo "<td>tex</td>";
                             }
                     echo "</tr>";
                 }
