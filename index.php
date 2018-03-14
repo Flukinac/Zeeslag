a955b94cea25ce77ede6b8d6b87080200740d7fe
Steven Visser <sevisser1@gmail.com>
2018-03-14 15:52:26 +0100

    laatste wijziging

--- index.php
+++ index.php
@@ -4,17 +4,9 @@
             echo "<table>";
                 for($y=0;$y<11;$y++){ 
                     echo "<tr border='2px' bordercolor='yellow'>";
-                            for($x=0;$x<11;$x++){
-                                if($x!=0&&$y!=10){
+                            for($x=0;$x<10;$x++){
+                               
                                     echo "<td bgcolor=#2B83A1 bordercolor='yellow' border='2px' style='height: 50px; width: 50px; text-align:center;'>tex</td>";
-                                }else{
-                                    echo "<td style='height: 50px; width: 50px; text-align:center; bgcolor='grey' bordercolor='black' border='1px''>";
-                                    if($y==10){
-                                        echo $x;
-                                    }else{
-                                        echo $y;}
-                                    echo "</td>";
-                                }
                             }
                     echo "</tr>";
                 }
