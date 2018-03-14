e8375c9a15fc3e858d734f6feed9ae0e0bf23c21
Steven Visser <sevisser1@gmail.com>
2018-03-14 16:58:48 +0100

    vakjes hebben nu een id

--- index.php
+++ index.php
@@ -5,10 +5,11 @@
     <body>
        <?php
             echo "<table>";
-                for($y=0;$y<11;$y++){ 
+                for($y=0;$y<10;$y++){ 
                     echo "<tr>";
                             for($x=0;$x<10;$x++){
-                                    echo "<td>tex</td>";
+                                    $cor = $y*10+$x;                        //maakt unieke id codes aan met de x en y as
+                                    echo "<td id=".$cor.">".$cor."</td>";   //creert de vakjes en voert de id waarde per vakje in
                             }
                     echo "</tr>";
                 }
@@ -17,4 +18,3 @@
     </body>  
 </html>
 
-<!--nu gaat het goed-->
