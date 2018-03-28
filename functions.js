0a0638fc9b2e299ec99bfba190ce44ca7cba8703
Raymon den Hartogh <raydenhartogh@gmail.com>
2018-03-28 15:17:16 +0200

    update

--- functions.js
+++ functions.js
@@ -1,21 +1,35 @@
+
 function newuser(){
-    var gebruiker = {}
-    gebruiker.naam = document.getElementById("username").value;
-    gebruiker.email = document.getElementById("email").value;
-    gebruiker.password = document.getElementById("pwd").value;
-    //alert("Nieuwe user aangemaakt");
+//    var gebruiker = {};
+//    gebruiker.naam = document.getElementById("username").value;
+//    gebruiker.email = document.getElementById("email").value;
+//    gebruiker.password = document.getElementById("pwd").value;
+    
+//    var naam = "raymon";
     var xml = new XMLHttpRequest();
+     
     xml.onreadystatechange = function (){
         if(xml.readyState === 4 && xml.status === 200){
+           
             console.log(this.responseText);
-            console.log(xml);
         }
-    var userdata = JSON.stringify(gebruiker);
-    xml.open("POST", "newuser.php", true);
+    };
+    
+    var userdata = JSON.stringify('raymon');
+    xml.open("POST", 'newuser.php', true);
     xml.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     xml.send("gebruiker="+userdata);
-    };
 }
+
+
+
+
+
+
+
+
+
+
 
 
 function newrekening(){
