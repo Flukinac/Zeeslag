0461061bde342345b76883a079b933b0e0c4506d
alexanderlexis <alexander.lexis@gmail.com>
2018-03-28 13:25:06 +0200

    eerste js functie

--- functions.js
+++ functions.js
@@ -1,7 +1,21 @@
 function newuser(){
-    alert("Nieuwe user aangemaakt");
+    var gebruiker = {}
+    gebruiker.naam = document.getElementById("username").value;
+    gebruiker.email = document.getElementById("email").value;
+    gebruiker.password = document.getElementById("pwd").value;
+    //alert("Nieuwe user aangemaakt");
+    var xml = new XMLHttpRequest();
+    xml.onreadystatechange = function (){
+        if(xml.readyState === 4 && xml.status === 200){
+            console.log(this.responseText);
+            console.log(xml);
+        }
+    var userdata = JSON.stringify(gebruiker);
+    xml.open("POST", "newuser.php", true);
+    xml.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xml.send("gebruiker="+userdata);
+    };
 }
-
 
 
 function newrekening(){
---
+++ newuser.php
@@ -0,0 +1,4 @@
+<?php
+//    include "classes.php";
+echo "nieuwe user aangemaakt";
+
