271ccd6ed16283a6a71b692338b9357ce7e13338
Steven Visser <sevisser1@gmail.com>
2018-03-31 22:06:22 +0200

    css stuk verder

--- index2.php
+++ index2.php
@@ -19,32 +19,31 @@ and open the template in the editor.
         <div class="titelDiv"><h1 class="titel">RSA Bank</h1></div>
         <div class="SignUp">
             <span class="aanmeldTitel"><h3>Maak gebruiker aan</h3></span>
-            <input class="signUpText" size=50  type=text id=username name=username placeholder=Gebruikersnaam><br>
-            <input class="signUpText" size=50 type=text id=email name=email placeholder=Email><br>
-            <input class="signUpText" size=51  type=password id=pwd name=pwd placeholder=Wachtwoord><br>
-            <input class="signUpText" size=50 type=text id=rechten name=rechten placeholder=Rechten><br>
+            <input class="signUpText" size=30  type=text id=username name=username placeholder=Gebruikersnaam><br>
+            <input class="signUpText" size=30 type=text id=email name=email placeholder=Email><br>
+            <input class="signUpText" size=31  type=password id=pwd name=pwd placeholder=Wachtwoord><br>
+            <input class="signUpText" size=30 type=text id=rechten name=rechten placeholder=Rechten><br>
             <input class="signUpText" type=button value="Maak gebruiker aan"onclick="newuser();"><br>
         </div>
-        <hr>
-        <hr>
+       
         
         <div class="maakRekeningDiv">
-            <span class="aanmeldTitel"<h3>Maak rekening aan</h3><br>
-            <input type=text size=50 id=rekeningnr name=rekeningnr placeholder=Rekeningnummer><br>
-            <input type=text size=50 id=saldo name=saldo placeholder=Saldo><br>
-            <input type=password size=51 id=pincode name=pincode placeholder=Pincode><br>
+            <span class="aanmeldTitel"><h3>Maak rekening aan</h3></span><br>
+            <input type=text size=30 id=rekeningnr name=rekeningnr placeholder=Rekeningnummer><br>
+            <input type=text size=30 id=saldo name=saldo placeholder=Saldo><br>
+            <input type=password size=31 id=pincode name=pincode placeholder=Pincode><br>
             <input type=button value="Maak rekening aan"onclick="newrekening();"><br>
         </div>
-        <hr>
         
         
+        <div class="transactieDiv">
         <h3>Maak transactie</h3>
-        <input type=text id=ontvanger name=ontvanger placeholder="Rekeningnummer ontvanger"><br>
-        <input type=text id=verzender name=verzender placeholder="Rekeningnummer verzender"><br>
-        <input type=text id=bedrag name=bedrag placeholder=Bedrag><br>
-        <input type=text id=opmerking name=opmerking placeholder=Opmerking><br>
+        <input type=text size=30 id=ontvanger name=ontvanger placeholder="Rekeningnummer ontvanger"><br>
+        <input type=text size=30 id=verzender name=verzender placeholder="Rekeningnummer verzender"><br>
+        <input type=text size=30 id=bedrag name=bedrag placeholder=Bedrag><br>
+        <input type=text size=30 id=opmerking name=opmerking placeholder=Opmerking><br>
         <input type=button value="Maak transactie aan"onclick="newtransactie();"><br>
-        
+        </div>
         
         
         
